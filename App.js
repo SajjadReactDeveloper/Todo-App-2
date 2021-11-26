5209ab09121ac3eaad95d7a010f97d257ca526d1
Sunny0099$ <sufyanabbas306@gmail.com>
2021-11-26 22:54:24 +0500

    Display Item

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { FlatList, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import { FlatList, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   const [text, setText] = React.useState('');
@@ -29,7 +29,16 @@ export default function App() {
         </Pressable>
       </View>
       <FlatList 
+        style = {{width: '100%', paddingTop: 20}}
         data = {item}
+        renderItem = {({item}) => (
+          <TouchableOpacity 
+            key = {item.key}
+            style = {styles.touchable}
+          >
+            <Text>{item.data}</Text>
+          </TouchableOpacity>
+        )}
       />
       <StatusBar style="auto" />
     </View>
@@ -53,5 +62,14 @@ const styles = StyleSheet.create({
     padding: 10,
     backgroundColor: 'lightblue',
     borderRadius: 5,
+  },
+  touchable: {
+    backgroundColor: 'green',
+    padding: 5,
+    margin: 5,
+    borderRadius: 15,
+    width: '90%',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   }
 });
