4a58e9fc277198c8ee1fe32d3987b1c8058795c1
Sunny0099$ <sufyanabbas306@gmail.com>
2021-11-26 22:45:21 +0500

    Basic Structure

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [text, setText] = React.useState('');
+  const [item, setItem] = React.useState([]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Todo App</Text>
+      <View style = {{flexDirection: 'row', marginTop: 10}}>
+        <TextInput style = {styles.input} placeholder = "Enter Product Name" placeholderTextColor = "green"/>
+        <Pressable style = {styles.pressable}><Text style = {{color: '#fff'}}>Add</Text></Pressable>
+      </View>
+      <FlatList 
+        data = {}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +26,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
   },
+  input: {
+    width: 200,
+    borderBottomWidth: 1,
+    borderBottomColor: 'blue'
+  },
+  pressable: {
+    marginLeft: 10,
+    padding: 10,
+    backgroundColor: 'lightblue',
+    borderRadius: 5,
+  }
 });
